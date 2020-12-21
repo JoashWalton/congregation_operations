b6a922a71170cb452bcf065311397cbb3c18c34b
Josh Walton <joash.tron@gmail.com>
2020-12-20 22:56:28 -0600

    Docuementation for Soul, Publisher, and Contact Information Models

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,2 +1,12 @@
+# Class for describing a publisher with data that is specific to a base member of a congregation
+#
+# Attributes:
+# * unbaptized - boolean, teue if the publisher has been announced as 
+# * unbaptized_date - date, date of announcement, month, date, year
+# * baptized - boolean, true if the publisher has been baptized
+# * baptism_date - date, date of baptism, month, date, year
+# * other_sheep - boolean, true if the publisher does **not** partake of the emblems
+# * anointed - boolean, true if the publisher does partake of the emblems and identifies as such
+#
 class Publisher < ApplicationRecord
 end
--- app/models/soul.rb
+++ app/models/soul.rb
@@ -1,2 +1,12 @@
+# Class for describing a soul - the base data of a person
+#
+# Attributes:
+# * first_name - string, the first name 
+# * middle_name - string, the middle name
+# * last_name - string, the last name
+# * birth_date - date, date of birth, month, date, year
+# * gender - string, male or female
+# * death_date - date, date of death, month, date, year
+#
 class Soul < ApplicationRecord
 end
