74008085ce5b60f1a001d034f08afe3d915a80b1
Joshua Walton <joash.tron@gmail.com>
2021-04-01 14:24:49 -0500

    Public Talk Outline Name Fromatting - Date Selection Extension
    https://apidock.com/rails/String/titleize
    
    https://api.rubyonrails.org/classes/ActionView/Helpers/DateHelper.html#method-i-date_select

--- app/models/public_talk_outline.rb
+++ app/models/public_talk_outline.rb
@@ -8,4 +8,12 @@
 #
 class PublicTalkOutline < ApplicationRecord
   has_and_belongs_to_many :public_speakers
+
+  before_save :titleize_public_talk_title
+
+  def titleize_public_talk_title
+    return unless outline_name.present?
+
+    self.outline_name = outline_name.titleize
+  end
 end
